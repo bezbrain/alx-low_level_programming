5c2824bd551ac511bfb0665dfdc521ab3af5c214
bezbrain <belloquadriolawale@gmail.com>
2022-12-08 07:29:31 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /**
- * printing a statement without using the printf keyword
- * the function also returned zero
+ * main: printing a statement without using the printf keyword
+ * return: the function also returned zero
  */
 int main(void)
 {
