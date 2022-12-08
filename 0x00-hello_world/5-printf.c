aafd1c9ed4536d56a070d81e2b5402b25be2eeba
bezbrain <belloquadriolawale@gmail.com>
2022-12-08 08:01:18 -0800

    Task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,7 +1,11 @@
 #include <stdio.h>
 
+/**
+ *main : Printing proper grammar
+ *Return: 0 is returned properly
+ */
 int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art,\n");
-	return 0;
+	return (0);
 }
