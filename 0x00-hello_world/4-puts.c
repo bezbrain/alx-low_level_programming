0b0bb70794a295b5e544b537c632d12e589711bb
bezbrain <belloquadriolawale@gmail.com>
2022-12-08 07:15:03 -0800

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+/**
+ * printing a statement without using the printf keyword
+ * the function also returned zero
+ */
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
