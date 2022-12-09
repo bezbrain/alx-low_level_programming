a8bdb88654f3498f1d56960a68a53672af5ab9b8
bezbrain <belloquadriolawale@gmail.com>
2022-12-08 23:26:45 -0800

    Task 8

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <unistd.h>
+
+/**
+ * main - printing and that piece of art is useful"
+ * - Dora Korpar, 2015-10-19
+ * Return: 1 if exited properly, not 1 if otherwise
+ */
+int main(void)
+{
+	write("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n");
+	return (1);
+}
